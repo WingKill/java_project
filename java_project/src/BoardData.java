b2eee5509871efb6d4aadfb0e24b0a565417fa62
601-16 <601-16@DESKTOP-J2TK4C1>
2024-02-08 09:10:29 +0900

    클래스만 잔뜩 만들어놓기

--- java_project/src/BoardData.java
+++ java_project/src/BoardData.java
@@ -3,44 +3,14 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 // 작성, 수정, 삭제 담당. 근데 데이터 보관은 아닌거 같은데... 분리해야겠다 나중에
-public class BoardData{
+public class BoardData extends Thread{
 	private String boardSubject;
 	private String boardwriter;
 	private String boardPostMain;
 	private LocalDate boardDate;
+	private String isdone; // 진행 여부를 판단하는 String 
 	
 	Scanner sc = new Scanner(System.in);
-	
-	// 생성하기.
-	public BoardData() {
-		System.out.println("-----------------------------");
-		System.out.println("작성자  : ");
-		String boardwriter = sc.next(); 
-		System.out.println("-----------------------------");
-		System.out.print("게시글 제목 : " );
-		String boardSubject = sc.next();
-		System.out.println("-----------------------------");
-		System.out.print("게시글 내용 : ");
-		String boardPostMain = sc.nextLine();
-		System.out.println("-----------------------------");
-		System.out.println("| 1. 작성 | 2. 취소 |");
-		System.out.println("-----------------------------");
-		System.out.print("입력되는 값(1,2 중 하나) : ");
-		String chooseNum = sc.next();
-		if(chooseNum == "1") {
-			this.boardSubject = boardSubject;
-			this.boardwriter = boardwriter;
-			this.boardPostMain = boardPostMain;
-			this.boardDate = LocalDate.now();
-			sc.close();
-			BoardMainPage mainPage = new BoardMainPage();
-		}else {
-			System.out.println("메인 화면으로 돌아갑니다.");
-			sc.close();
-			BoardMainPage mainPage = new BoardMainPage();
-		}		
-		
-	}
 	
 	public void editPost(String boardwriter) {
 		if(this.boardwriter.equals(boardwriter)) {
@@ -55,15 +25,43 @@ public class BoardData{
 			System.out.println("-----------------------------");
 			System.out.println("| 1. 수정 | 2. 취소 |");
 			System.out.print("입력되는 값(1,2 중 하나) : ");
-			String chooseNum = sc.next();
+			int chooseNum = sc.nextInt();
 		}else {
 			System.out.println("정상적인 접근이 아닙니다. 메인 화면으로 돌아갑니다.");
-			BoardMainPage mainPage = new BoardMainPage();
 			return;
 		}
 	}
 	
 	public void deletePost() {
 		
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("-----------------------------");
+		System.out.println("작성자  : ");
+		String boardwriter = sc.next(); 
+		System.out.println("-----------------------------");
+		System.out.print("게시글 제목 : " );
+		String boardSubject = sc.next();
+		System.out.println("-----------------------------");
+		System.out.print("게시글 내용 : ");
+		String boardPostMain = sc.nextLine();
+		System.out.println("-----------------------------");
+		System.out.println("| 1. 작성 | 2. 취소 |");
+		System.out.println("-----------------------------");
+		System.out.print("입력되는 값(1,2 중 하나) : ");
+		int chooseNum = sc.nextInt();
+		if(chooseNum == 1) {
+			isdone = chooseNum + ". 작성";
+			this.boardSubject = boardSubject;
+			this.boardwriter = boardwriter;
+			this.boardPostMain = boardPostMain;
+			this.boardDate = LocalDate.now();
+			sc.close();
+		}else {
+			System.out.println("메인 화면으로 돌아갑니다.");
+			sc.close();
+		}		
 	}
 }
--- java_project/src/BoardMainPage.java
+++ java_project/src/BoardMainPage.java
@@ -2,77 +2,48 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class BoardMainPage {
+public class BoardMainPage extends Thread{
 	private List<BoardData> listMain = new ArrayList<BoardData>();
 	public BoardMainPage() {
-		Scanner sc = new Scanner(System.in); // 입력받기
 		
-		System.out.println("-------------------------------------------------------");	
-		System.out.println("  |  전체 목록   | 게시글 제목    |  게시글 내용   | 게시글 작성자  |");
-								//	10    | 	13    |   13	 |   10
-								// 전체 목록, 게시글 작성자를 제외하고 각각 5를 뺀 숫자를 초과하면 나머지 부분은 ...으로 표시
-								// 전체 목록은 수치로 표시
-		System.out.println("-------------------------------------------------------");
-		// 변수를 넣을 자리 구분해놓음
-		if(true) {
-			System.out.println("  |         |             |          |          |" );
-		}else {
-			System.out.println("  |"+
-					"|"+
-					"|"+
-					"|"+
-					"|" );
-		}		
-		
-		System.out.println("-------------------------------------------------------");
-		System.out.println("              | 1. 내 게시글 목록 | 2. 게시글 작성 | ");
-		
-		System.out.print("선택할 영역(1,2 중 하나) : ");
-		String enterString = sc.next();
-		if(enterString == "1") {
-			System.out.println("-----------------------ㅡMY PAGE-----------------------");
-			BoardMyPage myPage = new BoardMyPage();
-		}else if(enterString == "2") {
-			
-		}else {
-			
-			
-		}
 	}
 	
 	private void returnMainPage() {
-		
 		System.out.println("잘못 입력하셨습니다. 메인 페이지로 다시 연결합니다.");
 		System.out.println("----------------------restart------------------------");
-		
 	}
 
-
-	public static void main(String[] args) {
+	
+	@Override
+	public void run() {
 		Scanner sc = new Scanner(System.in); // 입력받기
-		
-		System.out.println("-------------------------------------------------------");	
+
+		System.out.println("-------------------------------------------------------");
 		System.out.println("  |  전체 목록   | 게시글 제목    |  게시글 내용   | 게시글 작성자  |");
-								//	10    | 	13    |   13	 |   10
-								// 전체 목록, 게시글 작성자를 제외하고 각각 5를 뺀 숫자를 초과하면 나머지 부분은 ...으로 표시
-								// 전체 목록은 수치로 표시
+		// 10 | 13 | 13 | 10
+		// 전체 목록, 게시글 작성자를 제외하고 각각 5를 뺀 숫자를 초과하면 나머지 부분은 ...으로 표시
+		// 전체 목록은 수치로 표시		
 		System.out.println("-------------------------------------------------------");
 		// 변수를 넣을 자리 구분해놓음
-		if(true) {
-			System.out.println("  |         |             |          |          |" );
-		}else {
-			System.out.println("  |"+
-					"|"+
-					"|"+
-					"|"+
-					"|" );
-		}		
-		
+		if (true) {
+			System.out.println("  |         |             |          |          |");
+		} else {
+			System.out.println("  |" + "|" + "|" + "|" + "|");
+		}
+
 		System.out.println("-------------------------------------------------------");
 		System.out.println("              | 1. 내 게시글 목록 | 2. 게시글 작성 | ");
-		
+
 		System.out.print("선택할 영역(1,2 중 하나) : ");
 		String enterString = sc.next();
-		
+		if (enterString == "1") {
+			System.out.println("-----------------------ㅡMY PAGE-----------------------");
+			this.run();
+		} else if (enterString == "2") {
+
+		} else {
+
+		}
 	}
+	
 }
